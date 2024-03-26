3b4e8fb8e2f4075c7721a895370b25dc7ae4b6d9
NelsonKinyanjui <nelsonkinyanjui246@gmail.com>
2024-03-26 21:59:26 +0300

    add results with nested if statements

--- assets/rps.js
+++ assets/rps.js
@@ -1,4 +1,6 @@
-let randomNumber = Math.random();
+ 
+function getComputerChoice(){
+    let randomNumber = Math.random();
     if (randomNumber > 0 && randomNumber < 1 / 3){
             return 'rock';
 
@@ -11,3 +13,49 @@ let randomNumber = Math.random();
         return 'scissors';
 
 }
+
+}
+ function playRound (playerSelection , computerSelection){
+    let result = '';
+
+    if (playerSelection === 'rock'){
+        if(computerSelection === 'rock'){
+            result = 'You tie.'
+        }
+        else if (computerSelection === 'paper'){
+            result = 'You lose.'
+
+        }
+        else if (computerSelection === 'scissors'){
+            result = 'You win.'
+        }
+    }
+    else if (playerSelection === 'paper'){
+        if(computerSelection === 'rock'){
+            result = 'You win.'
+        }
+        else if (computerSelection === 'paper'){
+            result = 'You tie.'
+
+        }
+        else if (computerSelection === 'scissors'){
+            result = 'You lose.'
+        }
+    }
+    else if (playerSelection === 'scissors'){
+        if(computerSelection === 'rock'){
+            result = 'You lose.'
+        }
+        else if (computerSelection === 'paper'){
+            result = 'You win.'
+
+        }
+        else if (computerSelection === 'scissors'){
+            result = 'You tie.'
+        }
+    }
+    playerSelection.toLowerCase() = prompt('Your choice' , '');
+    computerSelection = getComputerChoice();
+
+    return 
+ }
